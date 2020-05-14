612aaf7913f503a35840b8ffe96159468d8d2e2d
kiruthiharan <kiruthiharan@gmail.com>
2020-05-14 19:30:19 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PerfectScrollbarModule, PERFECT_SCROLLBAR_CONFIG, PerfectScrollbarConfigInterface} from 'ngx-perfect-scrollbar';
+
+import { AppComponent } from './app.component';
+
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+    suppressScrollX: true,
+    wheelPropagation: false
+  };
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
+    NgbModule.forRoot(),
+    PerfectScrollbarModule
+  ],
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    },
+    { provide: PERFECT_SCROLLBAR_CONFIG, useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+
+// COMPONENTS
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NotificationSidebarComponent } from './notification-sidebar/notification-sidebar.component';
+
+// DIRECTIVES
+import { ToggleFullscreenDirective } from './directives/toggle-fullscreen.directive';
+import { SidebarDirective } from './directives/sidebar.directive';
+import { SidebarLinkDirective } from './directives/sidebarlink.directive';
+import { SidebarListDirective } from './directives/sidebarlist.directive';
+import { SidebarAnchorToggleDirective } from './directives/sidebaranchortoggle.directive';
+import { SidebarToggleDirective } from './directives/sidebartoggle.directive';
+
+@NgModule({
+    exports: [
+        CommonModule,
+        FooterComponent,
+        NavbarComponent,
+        SidebarComponent,
+        NotificationSidebarComponent,
+        ToggleFullscreenDirective,
+        SidebarDirective,
+        NgbModule,
+    ],
+    imports: [
+        RouterModule,
+        CommonModule,
+        NgbModule,
+        PerfectScrollbarModule
+    ],
+    declarations: [
+        FooterComponent,
+        NavbarComponent,
+        SidebarComponent,
+        NotificationSidebarComponent,
+        ToggleFullscreenDirective,
+        SidebarDirective,
+        SidebarLinkDirective,
+        SidebarListDirective,
+        SidebarAnchorToggleDirective,
+        SidebarToggleDirective
+    ]
+})
+export class SharedModule { }
