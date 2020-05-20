82f37e12cd8b1dfda5f49a16adcdd57efb3a3d7e
kiruthiharan <kiruthiharan@gmail.com>
2020-05-20 19:46:29 +0530

    login validation

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  email: string;
+  password: string;
+  submitted = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    this.email = form.value.email;
+    this.password = form.value.password;
+
+    console.log(this.email);
+    console.log(this.password);
+    this.submitted = true;
   }
 
 }
