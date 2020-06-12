a2e0ef954c9b1375ce46833ea8914d4cbd40a66a
kiruthiharan <kiruthiharan@gmail.com>
2020-06-12 21:25:50 +0530

    forgot and reset password impl

--- src/app/pages/auth/forgot-password/forgot-password.component.ts
+++ src/app/pages/auth/forgot-password/forgot-password.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'app/core/_services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,14 +12,18 @@ import { AuthService } from 'app/core/_services/auth.service';
 export class ForgotPasswordComponent implements OnInit {
 
   email: String;
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private toastr: ToastrService,
+    private spinner: NgxSpinnerService, private router: Router) { }
 
   ngOnInit() {
   }
 
   recoverPassword() {
     this.authService.sendPasswordResetLink(this.email).subscribe(data => {
-      console.log(data);
+      this.toastr.success('An email will been sent to you with the necessary instructions, please check your email to reset the password');
+      this.router.navigate(['/login'])
+    }, error => {
+      this.toastr.error(error);
     })
   }
 
--- src/app/pages/auth/reset-password/reset-password.component.ts
+++ src/app/pages/auth/reset-password/reset-password.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'app/core/_services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
@@ -9,18 +11,39 @@ import { AuthService } from 'app/core/_services/auth.service';
 })
 export class ResetPasswordComponent implements OnInit {
   resetToken: string;
-  password: string;
-  constructor(private route: ActivatedRoute, private authService: AuthService) { }
+  confirmPassword: string;
+  passwordForm: FormGroup;
+  constructor(private route: ActivatedRoute, private authService: AuthService,
+              private toastr: ToastrService, private router: Router) { }
 
   ngOnInit() {
+    this.passwordForm = new FormGroup({
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required),
+    }, this.passwordMatchValidator);
+
+
     this.route.queryParams.subscribe(params => {
       this.resetToken = params.token;
     })
   }
 
-  resetPassword() {
-    this.authService.resetPassword(this.password, this.resetToken).subscribe(data => {
-      
+  
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {mismatch: true};
+  }
+
+  get f() { return this.passwordForm.controls; }
+
+  onSubmit() {
+
+    const password = this.passwordForm.value.password;
+
+    this.authService.resetPassword(password, this.resetToken).subscribe(data => {
+      this.toastr.success('Your password has been changed successfully')
+      this.router.navigate(['login'])
+    }, error => {
+      this.toastr.error(error);
     })
   }
 }
