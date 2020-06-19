d80997d6b17ca2af06c35213c617432d8a6fd6cd
kiruthiharan <kiruthiharan@gmail.com>
2020-06-19 20:18:12 +0530

    fixed merger errors

--- src/app/pages/posts/posts-home/posts-home.component.ts
+++ src/app/pages/posts/posts-home/posts-home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-//import Post_modal from 'src/app/posts/post_modal';
+// import Post_modal from 'src/app/posts/post_modal';
 import Post_modal from '../post_modal';
-//import { DomSanitizer } from '@angular/platform-browser';
+// import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-posts-home',
@@ -12,47 +12,47 @@ export class PostsHomeComponent implements OnInit {
 
   public postList: Post_modal[];
   public new_post: Post_modal;
-  public url:any;
-  public flag:string="#009da0";
-  isShow=false;
-  
-  constructor(/*public sanitizer:DomSanitizer*/) { 
-    this.new_post=new Post_modal();
-    this.postList=[];
+  public url: any;
+  public flag = '#009da0';
+  isShow = false;
+
+  constructor(/*public sanitizer:DomSanitizer*/) {
+    this.new_post = new Post_modal();
+    this.postList = [];
   }
 
   ngOnInit() {
-    console.log("working")
+    console.log('working')
   }
 
-  onsubmit(value: any){
-    //this.postList.push(this.new_post);
+  onsubmit(value: any) {
+    // this.postList.push(this.new_post);
 
-    //add to given position
-    this.new_post.postedDate=new Date();
-    this.postList.splice(0,0,this.new_post);
-    this.new_post=new Post_modal();
-    //console.log(value);
+    // add to given position
+    this.new_post.postedDate = new Date();
+    this.postList.splice(0, 0, this.new_post);
+    this.new_post = new Post_modal();
+    // console.log(value);
   }
 
-  public likeCount(item: Post_modal){
-    //this.flag="red";
-    item.likeCount+=1;
-    //this.flag="#009da0";
+  public likeCount(item: Post_modal) {
+    // this.flag="red";
+    item.likeCount += 1;
+    // this.flag="#009da0";
   }
 
-  public uploadImage(event){
+  public uploadImage(event) {
     // const ref_element =event.target;
     // const photo=ref_element.files[0];
     // const url_el=URL.createObjectURL(photo);
     // this.url=this.sanitizer.bypassSecurityTrustResourceUrl(url_el);
     // this.new_post.imageURL=this.url;
 
-    if(event.target.files){
-      var reader= new FileReader();
+    if (event.target.files) {
+      let reader = new FileReader();
       reader.readAsDataURL(event.target.files[0]);
-      reader.onload=(event:any)=>{
-        this.new_post.imageURL=event.target.result;
+      reader.onload = (event: any) => {
+        this.new_post.imageURL = event.target.result;
       }
     }
   }
@@ -60,7 +60,7 @@ export class PostsHomeComponent implements OnInit {
   // public postStory(){
   // }
 
-  public toggleList(item){
-    item.isShow=!item.isShow;
+  public toggleList(item) {
+    item.isShow = !item.isShow;
   }
 }
