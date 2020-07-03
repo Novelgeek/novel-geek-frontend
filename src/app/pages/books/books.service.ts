b6b3ee8f8853f81dd392c4f81280f77d384e431e
Dhanushka Samarasinghe <dhanushkahere@gmail.com>
2020-07-03 17:26:57 +0530

    ui changes

--- src/app/pages/books/books.service.ts
+++ src/app/pages/books/books.service.ts
@@ -15,14 +15,19 @@ export class BooksService {
 
   getBooks(searchTerm) {
     this.name = searchTerm;
-    return this.http.get('https://www.googleapis.com/books/v1/volumes?q=' + encodeURIComponent(this.name) + '+intitle&maxResults=10&printType=books&key=' + this.apiKey);
+    return this.http.get('https://www.googleapis.com/books/v1/volumes?q=' + encodeURIComponent(this.name) + '+intitle&maxResults=12&printType=books&key=' + this.apiKey);
     // return this.http.get('https://www.googleapis.com/books/v1/volumes/oPIMmQEACAAJ?key=AIzaSyBXGwa67D5ZrrFuPP6YUNc9glwM8edWFck');
   }
   getBooksById(bookId) {
     this.bookId = bookId;
+    console.log("book id"+this.bookId);
     return this.http.get('https://www.googleapis.com/books/v1/volumes/' + this.bookId + '?key=AIzaSyBXGwa67D5ZrrFuPP6YUNc9glwM8edWFck');
   }
   getReviews(bookId){
-    return this.http.get('http://localhost:8080/book/test');
+    this.bookId = bookId;
+    return this.http.get('http://localhost:8080/book/getreviews/'+this.bookId);
+  }
+  addComment(comment) {
+    return this.http.post('http://localhost:8080/book/addcomment',comment);
   }
 }
