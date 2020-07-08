9675a3ccdac5b08d178328cfdfa180c349b5a763
Dhanushka Samarasinghe <dhanushkahere@gmail.com>
2020-07-09 01:51:34 +0530

    auction processing ui stuff

--- src/app/pages/auction/auction-process/auction-process.component.ts
+++ src/app/pages/auction/auction-process/auction-process.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auction-process.component.css']
 })
 export class AuctionProcessComponent implements OnInit {
-
+  users:any=[1,2,3,4,5]
   constructor() { }
 
   ngOnInit(): void {
