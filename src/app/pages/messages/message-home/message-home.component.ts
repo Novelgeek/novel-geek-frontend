25fa61bd7df15d0622209a0c7f327d4e7cfe32a5
archana1996 <archanaahil@gmail.com>
2020-07-29 14:10:58 +0530

    chat ui setup

--- src/app/pages/messages/message-home/message-home.component.ts
+++ src/app/pages/messages/message-home/message-home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 import { Chat } from '../../../../../examples/chat/chat.model';
+import { UserService } from 'app/core/_services/user.service';
 
 @Component({
   selector: 'app-message-home',
@@ -7,96 +8,21 @@ import { Chat } from '../../../../../examples/chat/chat.model';
   styleUrls: ['./message-home.component.scss']
 })
 export class MessageHomeComponent implements OnInit {
-  
-  chat: Chat[];
-  activeChatUser: string;
-  activeChatUserImg: string;
-  @ViewChild('messageInput', {static: false}) messageInputRef: ElementRef;
-  @ViewChild('chatSidebar', {static: false}) sidebar:ElementRef;
-  @ViewChild('contentOverlay', {static: false}) overlay:ElementRef;
 
-  messages = new Array();
-  item: number = 0;
-
-  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+  users: any = []
+  activeUser = null;
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getAllUsersExceptMe().subscribe(data => {
+      this.users = data;
+      this.activeUser = this.users[0]
+    })
   }
 
-  onAddMessage() {
-    if (this.messageInputRef.nativeElement.value != "") {
-      this.messages.push(this.messageInputRef.nativeElement.value);
-    }
-    this.messageInputRef.nativeElement.value = "";
-    this.messageInputRef.nativeElement.focus();
-  }
-
-  SetActive(event, chatId: string) {
-    var hElement: HTMLElement = this.elRef.nativeElement;
-    //now you can simply get your elements with their class name
-    var allAnchors = hElement.getElementsByClassName('list-group-item');
-    //do something with selected elements
-    [].forEach.call(allAnchors, function (item: HTMLElement) {
-      item.setAttribute('class', 'list-group-item no-border');
-    });
-    //set active class for selected item
-    event.currentTarget.setAttribute('class', 'list-group-item bg-blue-grey bg-lighten-5 border-right-primary border-right-2');
-
-    this.messages = [];
-
-    if (chatId === 'chat1') {
-      
-      this.activeChatUser = "Elizabeth Elliott";
-      this.activeChatUserImg = "assets/img/portrait/small/avatar-s-3.png";
-    }
-    else if (chatId === 'chat2') {
-      
-      this.activeChatUser = "Kristopher Candy";
-      this.activeChatUserImg = "assets/img/portrait/small/avatar-s-7.png";
-    }
-    else if (chatId === 'chat3') {
-      
-      this.activeChatUser = "Sarah Woods";
-      this.activeChatUserImg = "assets/img/portrait/small/avatar-s-8.png";
-    }
-    else if (chatId === 'chat4') {
-      
-      this.activeChatUser = "Bruce Reid";
-      this.activeChatUserImg = "assets/img/portrait/small/avatar-s-5.png";
-    }
-    else if (chatId === 'chat5') {
-      
-      this.activeChatUser = "Heather Howell";
-      this.activeChatUserImg = "assets/img/portrait/small/avatar-s-9.png";
-    }
-    else if (chatId === 'chat6') {
-      
-      this.activeChatUser = "Kelly Reyes";
-      this.activeChatUserImg = "assets/img/portrait/small/avatar-s-4.png";
-    }
-    else if (chatId === 'chat7') {
-      
-      this.activeChatUser = "Vincent Nelson";
-      this.activeChatUserImg = "assets/img/portrait/small/avatar-s-14.png";
-    }
-
-  }
-
-  onSidebarToggle() {
-    this.renderer.removeClass(this.sidebar.nativeElement, 'd-none');
-    this.renderer.removeClass(this.sidebar.nativeElement, 'd-sm-none');
-    this.renderer.addClass(this.sidebar.nativeElement, 'd-block');
-    this.renderer.addClass(this.sidebar.nativeElement, 'd-sm-block');
-    this.renderer.addClass(this.overlay.nativeElement, 'show');
-  }
-
-  onContentOverlay() {
-    this.renderer.removeClass(this.overlay.nativeElement, 'show');
-    this.renderer.removeClass(this.sidebar.nativeElement, 'd-block');
-    this.renderer.removeClass(this.sidebar.nativeElement, 'd-sm-block');
-    this.renderer.addClass(this.sidebar.nativeElement, 'd-none');
-    this.renderer.addClass(this.sidebar.nativeElement, 'd-sm-none');
-
+  setCurrentUser(user){
+    console.log(user)
+    this.activeUser = user
   }
 
 }
