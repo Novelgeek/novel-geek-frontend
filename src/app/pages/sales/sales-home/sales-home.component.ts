8c432b25fd52761cc7969ae80565717fbb5f8dc9
SanjaliNanyakkara <sanjalisupunnadi@gmail.com>
2020-08-01 04:26:32 +0530

    backend_integrated

--- src/app/pages/sales/sales-home/sales-home.component.ts
+++ src/app/pages/sales/sales-home/sales-home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import Selling_modal from '../selling_modal';
+import { HttpClient } from '@angular/common/http';
+import { SellingService } from 'app/core/_services/selling.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-sales-home',
@@ -7,22 +12,76 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./sales-home.component.scss']
 })
 export class SalesHomeComponent implements OnInit {
+  public myPosts: Selling_modal[];
+  public allPosts: Selling_modal[];
+  public new_post: Selling_modal;
+  
   create=false;
   contact=false;
+
   imagePath:any;
   selectedImage: File;
-
   telephone:any='';
 
-  constructor() {
-   }
+  constructor(private http: HttpClient, private sellingService: SellingService,
+    private toastr: ToastrService, private spinner: NgxSpinnerService) {
+    this.new_post = new Selling_modal();
+    this.myPosts = [];
+    this.allPosts = [];
+  }
   ngOnInit() {
-
+    this.spinner.show();
+    this.sellingService.getAllPosts().subscribe(response => {
+      this.allPosts = response;
+      this.spinner.hide();
+    }, error => {
+      this.spinner.hide();
+      this.toastr.error(error);
+    })
+    this.spinner.show();
+    this.sellingService.getMyPosts().subscribe(response => {
+      this.myPosts = response;
+      this.spinner.hide();
+    }, error => {
+      this.spinner.hide();
+      this.toastr.error(error);
+    })
   }
 
   onsubmit(Values: any) {
-    console.log(Values);
-    this.create = false;
+
+    this.spinner.show();
+    const newpost = new FormData();
+
+    newpost.append('title', Values.title);
+    newpost.append('description', Values.description)
+    newpost.append('merchantid', Values.merchantid)
+    newpost.append('price', Values.price)
+    if(this.contact){
+      newpost.append('telephone',Values.telephone)
+    }else{
+      newpost.append('telephone', "");
+    }
+    console.log(newpost);
+
+    newpost.append('file', this.selectedImage);
+    
+    this.sellingService.createPost(newpost)
+    .subscribe(response => {
+      this.new_post = response;
+
+      this.allPosts.splice(0, 0, this.new_post);
+      this.myPosts.splice(0, 0, this.new_post);
+
+      this.create = false;
+      this.spinner.hide();
+      this.toastr.success('Post created succesfully');
+
+    }, errorMsg => {
+      this.spinner.hide();
+      this.toastr.error('Unable to create post at the moment.');
+    })
+
     this.contact = false;
     this.imagePath = '';
   }
