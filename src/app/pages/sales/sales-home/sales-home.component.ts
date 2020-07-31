e9110732c3364f7f291d12ab1a958520b573ead8
SanjaliNanyakkara <sanjalisupunnadi@gmail.com>
2020-07-31 05:57:32 +0530

    create_sales_post

--- src/app/pages/sales/sales-home/sales-home.component.ts
+++ src/app/pages/sales/sales-home/sales-home.component.ts
@@ -7,9 +7,53 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./sales-home.component.scss']
 })
 export class SalesHomeComponent implements OnInit {
+  create=false;
+  contact=false;
+  imagePath:any;
+  selectedImage: File;
 
-  constructor() { }
+  telephone:any='';
+
+  constructor() {
+   }
   ngOnInit() {
+
   }
 
+  onsubmit(Values: any) {
+    console.log(Values);
+    this.create = false;
+    this.contact = false;
+    this.imagePath = '';
+  }
+
+  public uploadImage(event) {
+    if (event.target.files) {
+      const reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event: any) => {
+        this.imagePath = event.target.result;
+      }
+      this.selectedImage = event.target.files[0];
+    }
+  }
+
+  enableContact(){
+    this.contact = true;
+    this.telephone = '';
+  }
+  showMessage(){
+    this.contact =false;
+    this.telephone ='0000000000';
+  }
+
+  onOpen() {
+    this.create = true;
+  }
+
+  onClose() {
+    this.create = false;
+    this.imagePath = '';
+    this.contact = false;
+  }
 }
---
+++ src/app/pages/sales/sales-modal/sales-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-modal',
+  templateUrl: './sales-modal.component.html',
+  styleUrls: ['./sales-modal.component.scss']
+})
+export class SalesModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
