9097e1ab6d41c1349a62049737dc8157d626554b
archana1996 <archanaahil@gmail.com>
2020-07-31 18:35:09 +0530

    chat basic working

--- src/app/pages/messages/message-home/message-home.component.ts
+++ src/app/pages/messages/message-home/message-home.component.ts
@@ -1,28 +1,60 @@
-import { Component, OnInit, ElementRef, ViewChild, Renderer2 } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Renderer2, OnDestroy } from '@angular/core';
 import { Chat } from '../../../../../examples/chat/chat.model';
 import { UserService } from 'app/core/_services/user.service';
+import { ChatService } from 'app/core/_services/chat.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-message-home',
   templateUrl: './message-home.component.html',
   styleUrls: ['./message-home.component.scss']
 })
-export class MessageHomeComponent implements OnInit {
+export class MessageHomeComponent implements OnInit, OnDestroy {
 
   users: any = []
   activeUser = null;
-  constructor(private userService: UserService) { }
+  message: String = '';
+  messageHistory: any = [];
+
+  incoming: any = [];
+  outgoing: any = [];
+
+  messageSubscription: Subscription;
+
+  constructor(private userService: UserService, private chatService: ChatService) { }
+
+  ngOnDestroy(): void {
+    this.messageSubscription.unsubscribe();
+  }
 
   ngOnInit() {
     this.userService.getAllUsersExceptMe().subscribe(data => {
       this.users = data;
-      this.activeUser = this.users[0]
+      this.setCurrentUser(this.users[0]);
+    })
+
+
+  }
+
+  setCurrentUser(user) {
+    console.log(user)
+    this.activeUser = user
+    if (this.messageSubscription !== undefined) {
+      this.messageSubscription.unsubscribe();
+    }
+
+    this.messageSubscription = this.chatService.get(this.activeUser.id).subscribe(data => {
+      this.messageHistory = data;
+      this.outgoing = this.messageHistory.filter(message => message.to === this.activeUser.id)
+      this.incoming = this.messageHistory.filter(message => message.to !== this.activeUser.id)
+      console.log(data)
     })
   }
 
-  setCurrentUser(user){
-    console.log(user)
-    this.activeUser = user
+  sendMessage() {
+    console.log(this.message);
+    this.chatService.sendMessage(this.activeUser.id, this.message)
+    this.message = '';
   }
 
 }
